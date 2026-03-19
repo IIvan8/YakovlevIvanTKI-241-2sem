cd0a2aed0be4c8e3f015fe4350b96bdff080f431
IIvan8 <vanya.van28@mail.ru>
2026-03-19 23:05:22 +0300

    выполнил задание 2

---
+++ yazprog2/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <list>
+#include <algorithm>
+#include <iterator>
+
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+
+    std::cout << "Введите количество элементов, кратное 4: ";
+
+    std::list<int> L;
+    std::copy(
+        std::istream_iterator<int>(std::cin),
+        std::istream_iterator<int>(),
+        std::back_inserter(L)
+    );
+
+    if (L.size() % 4 != 0)
+    {
+        std::cout << "Ошибка: количество элементов должно быть кратно 4\n";
+        return 0;
+    }
+
+    auto mid = L.begin();
+    std::advance(mid, L.size() / 2); 
+
+
+    for (auto i = L.begin(); i != mid; )
+    {
+        L.erase(i++); 
+        if (i != mid) ++i; 
+    }
+
+    std::cout << "Результат: ";
+    std::copy(L.begin(), L.end(),
+        std::ostream_iterator<int>(std::cout, " "));
+
+    return 0;
+}
