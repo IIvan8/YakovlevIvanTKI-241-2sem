e5d00c46efb4909bfa371e210e32c38f5e907a14
IIvan8 <vanya.van28@mail.ru>
2026-03-22 23:37:06 +0300

    выполнил задание 4

---
+++ yazprog4/main.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <cmath>
+
+using namespace std;
+using namespace std::placeholders;
+
+struct less_abs : public function<bool(int, int)> {
+    bool operator()(int a, int b) const {
+        return abs(a) < abs(b);
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int K;
+    vector<int> V;
+
+    cout << "Введите целое число K (> 0): ";
+    cin >> K;
+
+    if (K <= 0) {
+        cout << "Ошибка: K должно быть больше 0" << endl;
+        return 1;
+    }
+
+    int n;
+    cout << "Введите количество элементов вектора: ";
+    cin >> n;
+
+    cout << "Введите " << n << " целых чисел через пробел: ";
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        V.push_back(x);
+    }
+
+    cout << "Исходный вектор: ";
+    for (int val : V) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    auto new_end = remove_if(V.begin(), V.end(),
+        bind(less_abs(), _1, K)); 
+
+    V.erase(new_end, V.end());
+
+    cout << "Размер преобразованного вектора: " << V.size() << endl;
+
+    cout << "Элементы преобразованного вектора: ";
+    if (V.empty()) {
+        cout << "вектор пуст";
+    }
+    else {
+        for (int val : V) {
+            cout << val << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
